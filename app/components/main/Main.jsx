fdce05f3b2c7eddf3ddbcdb94397765f47960268
ciroubao <1058426807@qq.com>
2018-11-16 18:50:46 +0800

    movies

---
+++ app/components/main/Main.jsx
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+import { Row, Col } from 'antd';
+
+class Main extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			movies:[],
+			name:''
+		}
+	}
+	componentWillMount(){
+		axios.get('../../app/json/threaders.json')
+		  .then(response=>{
+			this.setState({
+				movies:response.data.subjects
+			});
+		})
+		.catch(error=>{
+
+		})
+	}
+
+	render(){
+		return(
+				<div>
+					{
+						this.state.movies.map((item,key)=>{
+							return (
+									<Col span={6} key={key}>
+										<img style={{height:'150px',width:'100px'}} src={item.images.small} />
+										<h3>{item.title}</h3>
+									</Col>
+							)
+						})
+					}
+
+				</div>
+			)
+	}
+}
+
+export default Main;
---
+++ app/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM,{render} from 'react-dom';
+import App from './components/app/App.jsx';
+
+render(
+
+	<App />,
+	document.getElementById("container")
+
+
+);
